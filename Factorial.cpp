a87bba56efe180d5a4f9c7dfa05a7d98d2fe05b5
HectorPimentel <al222627@alumnos.uacj.mx>
2024-10-29 11:36:11 -0600

    Se agrego la parte de cajero automatico y en el factorial la variable num se cambio de int a double para calculos mas grandes

--- Factorial.cpp
+++ Factorial.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 using namespace std;
 
-int fact(int n) {
-    /** Función recursiva para calcular el factorial**/
-    //En caso de introducir 0
+double fact(double n) {
+    /** Funcion recursiva para calcular el factorial**/
     if (n == 0) {
         return 1;
     }
@@ -12,19 +11,80 @@ int fact(int n) {
     return n * fact(n - 1);
 }
 
+void cajero() {
+    /**Función para el cajero**/
+    double saldo = 20000;
+    int opc;
+    double retiro;
+
+    do { //Menu de opciones
+        cout << "\nCajero Automatico\n";
+        cout << "1. Consultar saldo\n";
+        cout << "2. Retiro de efectivo\n";
+        cout << "3. Salir del Cajero\n";
+        cout << "Por favor, seleccione una opcion: ";
+        cin >> opc;
+
+        switch (opc) {
+        case 1:
+            cout << "Su saldo actual es de: " << saldo << endl;
+            break;
+        case 2:
+            cout << "Ingrese la cantidad a retirar: ";
+            cin >> retiro;
+            if (retiro > saldo) {
+                cout << "Fondos insuficientes. No se puede realizar el retiro." << endl;
+            }
+            else {
+                saldo -= retiro;
+                cout << "Retiro exitoso. Su saldo actual es de: " << saldo << endl;
+            }
+            break;
+        case 3:
+            cout << "Saliendo del modo cajero automatico." << endl;
+            break;
+        default:
+            cout << "Opcion no valida. Ingrese otra opcion." << endl;
+            break;
+        }
+    } while (opc != 3);
+}
+
 int main() {
-    /**Funcion Principal pide el numero a calcular el factorial**/
-    int num;
+    cout << "Hector Francisco Pimentel Resendez - 222627\n";
+    int sel;
+    do { //Menu Principal de opciones
+        cout << "\nMenu Principal\n";
+        cout << "1. Calcular Factorial\n";
+        cout << "2. Cajero Automatico\n";
+        cout << "3. Salir del Programa\n";
+        cout << "Seleccione una opcion: ";
+        cin >> sel;
 
-    cout << "Introduce un numero para calcular el factorial: ";
-    cin >> num;
-
-    if (num < 0) { //Verifica si se introudce un numero negativo
-        cout << "No se puede realizar factorial para numeros negativos." << endl;
-    } 
-    else {
-        cout << "El factorial de " << num << " es " << fact(num) << endl;
-    }
+        switch (sel) {
+        case 1: {
+            double num;
+            cout << "Introduce un numero para calcular el factorial: ";
+            cin >> num;
+            if (num < 0) {
+                cout << "No se puede realizar factorial para numeros negativos." << endl;
+            }
+            else {
+                cout << "El factorial de " << num << " es " << fact(num) << endl;
+            }
+            break;
+        }
+        case 2:
+            cajero();
+            break;
+        case 3:
+            cout << "Saliendo del programa..." << endl;
+            break;
+        default:
+            cout << "Opcion no valida. Ingrese otra opcion." << endl;
+            break;
+        }
+    } while (sel != 3);
 
     system("pause");
     return 0;
